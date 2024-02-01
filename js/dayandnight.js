470f723205ad3c7885482ba19e11a8f2eb9c2890
George <killyen444@gmail.com>
2024-02-01 09:23:44 +0800

    adjust the image root path

--- js/dayandnight.js
+++ js/dayandnight.js
@@ -7,10 +7,10 @@ const toggleButton = document.getElementById('day-night');
 toggleButton.onclick= function() {
     document.body.classList.toggle("night-mode")
     if (document.body.classList.contains("night-mode")){
-        toggleButton.src="../icon/icons8-sun-48.png";
+        toggleButton.src="./icon/icons8-sun-48.png";
         toggleButton.alt = "Switch to day mode";
     }  else{
-        toggleButton.src="../icon/icons8-moon-48.png";
+        toggleButton.src="./icon/icons8-moon-48.png";
         toggleButton.alt = "Switch to night mode";
     }
 }
